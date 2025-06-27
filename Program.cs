98be3d2ed072005a14256bc17543f9746d2886d6
Paul Sho <Cyberkiller.CK@gmail.com>
2025-06-27 17:16:16 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectDaikoku
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
